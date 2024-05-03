59875cc58190668e3bec15d5dc45707beedfdbb6
sree1411 <sreekanthsree141122@gmail.com>
2024-05-03 14:41:14 +0530

    normal and destructre

--- src/component/UserComponent.jsx
+++ src/component/UserComponent.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
 const UserComponent = (props) => {
-  console.dir(props)
+//   console.dir(props)
+// below two way i mentioned the props normal using and destring the object
+  const {firstname, lastname, email} = props.user
   return (
        <>
-       <h1>{props.user.firstname}</h1>
+       {/* <h1>{props.user.firstname}</h1>
        <h1>{props.user.lastname}</h1>
-       <h1>{props.user.email}</h1>
+       <h1>{props.user.email}</h1> */}
+
+
+       <h1>{firstname}</h1>
+       <h1>{lastname}</h1>
+       <h1>{email}</h1>
        
         
        </>
