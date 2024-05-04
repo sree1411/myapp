0907b9bec4315b96bb2f188ced06d1a25694d5d6
sree1411 <sreekanthsree141122@gmail.com>
2024-05-04 17:35:16 +0530

    form handling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
+ 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+
+ const [users, setUsers]= useState({
+   name:"",
+   email:"",
+   cars:"",
+   gender:"",
+   languages:[]
+ })
+ 
+
+const [updateUser, setUpdateuser]= useState('')
+
+
+const handleChange =(e)=>{
+   setUsers({...users, [e.target.name]:e.target.value})
+}
+
+ 
+
+  
+
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    alert(JSON.stringify(users,null,2))
+  }
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form action="" onSubmit={handleSubmit}>
+        <input type="text" placeholder='enter the name' name='name'  onChange={handleChange} value={users.name} />   <br/>
+        <input type="text" placeholder='enter the email' name='email'  onChange={handleChange} value={users.email}   /> <br/>
+        <label for="cars">Choose a car:</label>
+        <select id="cars" onChange={handleChange} name='cars'>
+          <option value="volvo">Volvo</option>
+          <option value="Benz">Benz</option>
+          <option value="Mahendra">Mahendra</option>
+          <option value="audi" selected>Audi</option>
+        
+        </select> <br/>
+
+
+        <div className="gender_section">
+              <input type="radio" id="male" name="gender" value='male' checked={users.gender ==="male"} onChange={handleChange}/>
+              <label for="male">Male</label><br/> 
+              <input type="radio" id="female" name="gender" value='female' checked={users.gender ==="female"}  onChange={handleChange} />
+              <label for="female">Female</label><br/> 
+              <input type="radio" id="other" name="gender" value='other' checked={users.gender ==="other"}  onChange={handleChange} />
+              <label for="other">Other</label><br/> 
+        </div>
+
+        <div className="checkbox_container">
+              <input type="checkbox" name="languages" id="" value='html' onChange={handleChange} />
+              <label htmlFor="html">HTML</label>
+              <input type="checkbox" name="languages" id="" value='css'   onChange={handleChange}/>
+              <label htmlFor="">Css</label>
+              <input type="checkbox" name="languages" id="" value='javascript' onChange={handleChange} />
+              <label htmlFor="">Java Script</label>
+        </div>
+        <button type='submit'> submit</button>
+        </form>
+
+        
     </div>
+
+   
+    
+    
+    </>
+    
   );
 }
 
