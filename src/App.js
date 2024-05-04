34d8d309ca522267a5fbd9a0a6baeba99b0b2fa2
sree1411 <sreekanthsree141122@gmail.com>
2024-05-04 21:36:12 +0530

    api calling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+const [users, setUsers]= useState([])
+
+  const apiHandler = async()=>{
+    
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await response.json()
+    setUsers(data)
+  
+
+  }
+ useEffect(()=>{
+  apiHandler()
+ }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {
+          users.map((item)=>{
+           return<>
+                <li>{item.name}</li>
+                </>
+          })
+        }
     </div>
   );
 }
