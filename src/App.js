5370b9665cc1bbc8ea6f247d66f8eb24c3bd5d25
sree1411 <sreekanthsree141122@gmail.com>
2024-05-05 10:47:56 +0530

    useeffect comibined with useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+
+
+    // useState lo if else condition base midha raste
+    // adi infinite loop loki veltadi , so alantapudu useEffect use 
+    //cheste dantlo dependency valla oka sari loop ayi agi poyi result estadi
+
+   const [name, setName] = useState('sreekanth')
+
+
+     useEffect(()=>{
+      if(name === "sreekanth"){
+        setName("Sreekanth Renati")
+      }else{
+        setName("sreekanth")
+      }
+     },[])
+
+     
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>  name is :{name}</h1>
     </div>
   );
 }
