48313ddf0ee1e6660c622719fa71b0cf15e78d24
sree1411 <sreekanthsree141122@gmail.com>
2024-05-05 07:32:53 +0530

    useContext

--- src/components/SecondComponent.jsx
+++ src/components/SecondComponent.jsx
@@ -3,12 +3,19 @@ import ThirdComponent from './ThirdComponent'
 import { useData } from '../useContext/ExampleContext'
 
 const SecondComponent = () => {
-  const {userDetails} = useData()
-  console.log(userDetails, "user")
+  const {userDetails, abc} = useData()
+  console.log(userDetails, "user", abc)
+   
+ 
   return (
       <>
          second component : <br/>
          <h1>{userDetails.name} </h1>
+         <h1> total Amount :{abc.map((item)=>{
+          return <>
+                 <li>{item}</li>
+              </>
+         })}</h1>
          <ThirdComponent/> <br/>
       </>
   )
--- src/useContext/ExampleContext.jsx
+++ src/useContext/ExampleContext.jsx
@@ -9,10 +9,11 @@ export const GiftProvider = ({ children }) => {
      email:"test@gmail",
      password: "1234"
   }
+  const abc= [12,22,33,52,35,34,57,77]
 
   return (
     <>
-      <GiftContext.Provider value={{ name, setName, userDetails }}>
+      <GiftContext.Provider value={{ name, setName, userDetails, abc }}>
         {children}
       </GiftContext.Provider>
     </>
