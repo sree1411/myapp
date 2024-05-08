6b84cfb4efb8e35e41e974efb07294a22982159b
sree1411 <sreekanthsree141122@gmail.com>
2024-05-08 16:00:56 +0530

    delete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 
 import './App.css';
-import HeroComponent from './components/HeroComponent';
-import Navbar from './components/Navbar';
+ 
 
 function App() {
   return (
     <div className="App">
        <h1> redux tool kit </h1>
-       <Navbar/>
-       <HeroComponent/>
+       
     </div>
   );
 }
