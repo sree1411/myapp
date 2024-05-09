a4e08cc176fff7f45489007de2a2c56b06f5b591
sree1411 <sreekanthsree141122@gmail.com>
2024-05-09 10:11:02 +0530

    api calling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+ 
 
 function App() {
+  const [details, setDetails] = useState([]);
+
+ 
+ const apiHanlder = async ()=>{
+   try {
+    const response = await fetch("https://fakestoreapi.com/products", {
+      method:"GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+    const data = await response.json()
+    setDetails(data)
+
+
+
+
+
+   } catch (error) {
+      console.log(error)
+   }
+ }
+  
+ useEffect(()=>{
+  apiHanlder()
+ }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {details.map((item) => {
+        return (
+          <>
+            <li>{item.title}</li>
+          </>
+        );
+      })}
     </div>
   );
 }
