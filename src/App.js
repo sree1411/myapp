aa8751103b38e30edb16a1ee872ad75924af82b6
sree1411 <sreekanthsree141122@gmail.com>
2024-05-09 10:38:07 +0530

    axios method

--- src/App.js
+++ src/App.js
@@ -6,11 +6,25 @@ import axios from "axios";
 function App() {
   const [details, setDetails] = useState([]);
 
-  const apiHandler = ()=>{
+  const apiHandler = async ()=>{
     
-      axios.get("https://fakestoreapi.com/products").then((res)=>{
-           setDetails(res.data)
+   
+     try {
+      const response = await axios.get("https://fakestoreapi.com/products", {
+        headers:{
+          "Content-Type": "application/json",
+        }
       })
+      //  if(response.status === 200){
+      //   setDetails(response.data)
+      //  }
+      const result = response.data
+      setDetails(result)
+     
+       
+     } catch (error) {
+        console.log(error)
+     }
      
   }
   
