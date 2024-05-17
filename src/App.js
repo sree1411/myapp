0a4ddd25512108f2cb9e456296022375f80a8bd8
sree1411 <sreekanthsree141122@gmail.com>
2024-05-17 11:15:58 +0530

    useEffect with fetch along with try and catch

--- src/App.js
+++ src/App.js
@@ -3,31 +3,31 @@ import "./App.css";
 import { useEffect } from "react";
 
 function App() {
+  const [students, setStudents] = useState([]);
 
-  
-
-  const [students, setStudents] = useState([])
-
-     useEffect(()=>{
-      fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res)=>res.json())
-      .then((data)=>setStudents(data)).catch((error)=>console.log(error.message))
-     }, []) 
-
-
-
-
+  const apiHandler = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
+      setStudents(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {
+    apiHandler();
+  }, []);
 
   return (
     <div className="App">
-      <h1> hello  </h1>
-      {
-        students.map((student, index)=>(
-           <div key={index}>
-             <li>{student.name}</li>
-           </div>
-        ))
-      }
+      <h1> hello </h1>
+      {students.map((student, index) => (
+        <div key={index}>
+          <li>{student.name}</li>
+        </div>
+      ))}
     </div>
   );
 }
