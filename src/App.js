a6ca298e5bdc7df671372d6461a1e7ff9b7117e1
sree1411 <sreekanthsree141122@gmail.com>
2024-05-17 11:10:33 +0530

    fetch Method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+
+  
+
+  const [students, setStudents] = useState([])
+
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res)=>res.json())
+    .then((data)=>setStudents(data)).catch((error)=>console.log(error.message))
+
+
+
   return (
     <div className="App">
       <h1> hello  </h1>
+      {
+        students.map((student, index)=>(
+           <div key={index}>
+             <li>{student.name}</li>
+           </div>
+        ))
+      }
     </div>
   );
 }
